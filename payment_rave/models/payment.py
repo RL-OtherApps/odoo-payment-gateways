5ef27e20165b4894d93afc46c9e7ee8531dd4a87
Ajepe <ajepebabatope@gmail.com>
2020-01-25 09:06:55 +0100

    [ADD] add payment_paystack

--- payment_rave/models/payment.py
+++ payment_rave/models/payment.py
@@ -15,41 +15,40 @@ _logger = logging.getLogger(__name__)
 
 
 class PaymentAcquirerStripe(models.Model):
-    _inherit = 'payment.acquirer'
+    _inherit = "payment.acquirer"
 
-    provider = fields.Selection(selection_add=[('rave', 'Rave')])
-    rave_public_key = fields.Char(
-        required_if_provider='rave', groups='base.group_user')
-    rave_secret_key = fields.Char(
-        required_if_provider='rave', groups='base.group_user')
-    environment = fields.Char(
-        required_if_provider='rave', groups='base.group_user')
+    provider = fields.Selection(selection_add=[("rave", "Rave")])
+    rave_public_key = fields.Char(required_if_provider="rave", groups="base.group_user")
+    rave_secret_key = fields.Char(required_if_provider="rave", groups="base.group_user")
+    environment = fields.Char(required_if_provider="rave", groups="base.group_user")
 
     @api.model
     def _get_rave_api_url(self):
         """ Rave URLs"""
-        if self.environment == 'prod':
-            return 'api.ravepay.co'
+        if self.environment == "prod":
+            return "api.ravepay.co"
         else:
-            return 'ravesandboxapi.flutterwave.com'
+            return "ravesandboxapi.flutterwave.com"
 
     @api.multi
     def rave_form_generate_values(self, tx_values):
         self.ensure_one()
         rave_tx_values = dict(tx_values)
         temp_rave_tx_values = {
-            'company': self.company_id.name,
-            'amount': tx_values['amount'],  # Mandatory
-            'currency': tx_values['currency'].name,  # Mandatory anyway
-            'currency_id': tx_values['currency'].id,  # same here
+            "company": self.company_id.name,
+            "amount": tx_values["amount"],  # Mandatory
+            "currency": tx_values["currency"].name,  # Mandatory anyway
+            "currency_id": tx_values["currency"].id,  # same here
             # Any info of the partner is not mandatory
-            'address_line1': tx_values.get('partner_address'),
-            'address_city': tx_values.get('partner_city'),
-            'address_country': tx_values.get('partner_country') and tx_values.get('partner_country').name or '',
-            'email': tx_values.get('partner_email'),
-            'address_zip': tx_values.get('partner_zip'),
-            'name': tx_values.get('partner_name'),
-            'phone': tx_values.get('partner_phone'),
+            "address_line1": tx_values.get("partner_address"),
+            "address_city": tx_values.get("partner_city"),
+            "address_country": tx_values.get("partner_country")
+            and tx_values.get("partner_country").name
+            or "",
+            "email": tx_values.get("partner_email"),
+            "address_zip": tx_values.get("partner_zip"),
+            "name": tx_values.get("partner_name"),
+            "phone": tx_values.get("partner_phone"),
         }
 
         rave_tx_values.update(temp_rave_tx_values)
@@ -57,57 +56,69 @@ class PaymentAcquirerStripe(models.Model):
 
 
 class PaymentTransactionRave(models.Model):
-    _inherit = 'payment.transaction'
+    _inherit = "payment.transaction"
 
     def _rave_verify_charge(self, data):
-        api_url_charge = 'https://%s/flwv3-pug/getpaidx/api/v2/verify' % (
-            self.acquirer_id._get_rave_api_url())
+        api_url_charge = "https://%s/flwv3-pug/getpaidx/api/v2/verify" % (
+            self.acquirer_id._get_rave_api_url()
+        )
         payload = {
-            'SECKEY': self.acquirer_id.rave_secret_key,
-            'txref': self.reference,
+            "SECKEY": self.acquirer_id.rave_secret_key,
+            "txref": self.reference,
         }
         headers = {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         }
 
-        _logger.info('_rave_verify_charge: Sending values to URL %s, values:\n%s',
-                     api_url_charge, pprint.pformat(payload))
-        r = requests.post(api_url_charge, headers=headers,
-                          data=json.dumps(payload))
+        _logger.info(
+            "_rave_verify_charge: Sending values to URL %s, values:\n%s",
+            api_url_charge,
+            pprint.pformat(payload),
+        )
+        r = requests.post(api_url_charge, headers=headers, data=json.dumps(payload))
         # res = r.json()
-        _logger.info('_rave_verify_charge: Values received:\n%s',
-                     pprint.pformat(r))
+        _logger.info("_rave_verify_charge: Values received:\n%s", pprint.pformat(r))
         return self._rave_validate_tree(r.json(), data)
 
     @api.multi
     def _rave_validate_tree(self, tree, data):
         self.ensure_one()
-        if self.state != 'draft':
+        if self.state != "draft":
             _logger.info(
-                'Rave: trying to validate an already validated tx (ref %s)', self.reference)
+                "Rave: trying to validate an already validated tx (ref %s)",
+                self.reference,
+            )
             return True
 
-        status = tree.get('status')
+        status = tree.get("status")
         amount = tree["data"]["amount"]
         currency = tree["data"]["currency"]
 
-        if status == 'success' and amount == data["amount"] and currency == data["currency"]:
-            self.write({
-                'date': fields.datetime.now(),
-                'acquirer_reference': tree["data"]["txid"],
-            })
+        if (
+            status == "success"
+            and amount == data["amount"]
+            and currency == data["currency"]
+        ):
+            self.write(
+                {
+                    "date": fields.datetime.now(),
+                    "acquirer_reference": tree["data"]["txid"],
+                }
+            )
             self._set_transaction_done()
             self.execute_callback()
             if self.payment_token_id:
                 self.payment_token_id.verified = True
             return True
         else:
-            error = tree['message']
+            error = tree["message"]
             _logger.warn(error)
-            self.sudo().write({
-                'state_message': error,
-                'acquirer_reference': tree["data"]["txid"],
-                'date': fields.datetime.now(),
-            })
+            self.sudo().write(
+                {
+                    "state_message": error,
+                    "acquirer_reference": tree["data"]["txid"],
+                    "date": fields.datetime.now(),
+                }
+            )
             self._set_transaction_cancel()
             return False
